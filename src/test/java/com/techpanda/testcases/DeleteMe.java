bca851e2b10bda3e8f85b7a5a622f07f8b8fb8ec
Khushboo Yadav <khushbooyadav373@gmail.com>
2022-07-29 15:42:31 +0530

    techPandaBranch

---
+++ src/test/java/com/techpanda/testcases/DeleteMe.java
@@ -0,0 +1,11 @@
+package com.techpanda.testcases;
+
+public class DeleteMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Example File");
+	}
+
+}
